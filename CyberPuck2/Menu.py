c618aa86026804fa2876976f1223432eb2cab659
EmmaDeste <emma_deste@hotmail.com>
2021-03-03 00:26:05 +0100

    buttons to choose the language + add verification to avoid bugs about dictionnary

--- CyberPuck2/Menu.py
+++ CyberPuck2/Menu.py
@@ -1,29 +1,65 @@
 import pygame
 from CyberPuck2.Slt_Menu import Slt_Menu
-from Multilingual import transL
+from CyberPuck2.Multilingual import transL, setL
 
 class Menu:
 
-    def __init__(self, application, *groups):
+    def addSpriteMenu(self, x, y , text, cmd):
+        font = pygame.font.SysFont('Helvetica', 29, bold=True)
+        mb = Slt_Menu(text, self.colors['normal'], font, x, y, 225, 50, cmd)
+        self.buttons.append(mb)
+        for groupe in self.groups:
+            groupe.add(mb)
+
+    def setFr(self):
+        setL("fr")
+        self.showMenu()
+
+    def setDe(self):
+        setL("de")
+        self.showMenu()
+
+    def setEs(self):
+        setL("es")
+        self.showMenu()
+
+    def setEn(self):
+        setL("en")
+        self.showMenu()
+
+    def showMenu(self):
+        for groupe in self.groups:
+            groupe.empty()  #pour éviter les superpositions de sprites
+        items = ((transL('Story'), self.application.story),
+                 (transL('Versus'), self.application.versus),
+                 (transL('Collection'), self.application.collection),
+                 (transL('Settings'), self.application.settings),
+                 (transL('Credit'), self.application.credit),
+                 (transL('Quit'), self.application.quit))
         self.colors = dict(normal=(200, 200, 0), survol=(0, 200, 200))
-        font = pygame.font.SysFont('Helvetica', 30, bold=True)
-        items = ((transL('Story'), application.story),
-                 (transL('Versus'), application.versus),
-                 (transL('Collection'), application.collection),
-                 (transL('Settings'), application.settings),
-                 (transL('Credit'), application.credit),
-                 (transL('Quit'), application.quit))
+
         x = 1366 / 2
         y = 100
         self.buttons = []
+
+        self.addSpriteMenu(1*x//5, y, "Français", self.setFr)
+        self.addSpriteMenu(3*x//5, y, "Deutsch", self.setDe)
+        self.addSpriteMenu(7*x//5, y, "Español", self.setEs)
+        self.addSpriteMenu(9*x//5, y, "English", self.setEn)
+
+        for text, cmd in items:
+            self.addSpriteMenu(x, y, text, cmd)
+            y += 120
+
+    def __init__(self, application, *groups):
+        self.groups = groups
+        self.application = application
+
         self.image = pygame.image.load("logo_test.png")
         pygame.display.set_icon(self.image)
-        for text, cmd in items:
-            mb = Slt_Menu(text, self.colors['normal'], font, x, y, 200, 50, cmd)
-            self.buttons.append(mb)
-            y += 120
-            for groupe in groups:
-                groupe.add(mb)
+
+        self.showMenu()
+
 
     def update(self, events):
         clicGauche, *_ = pygame.mouse.get_pressed()
--- CyberPuck2/Multilingual.py
+++ CyberPuck2/Multilingual.py
@@ -1,4 +1,4 @@
-currL = 'en'
+currL = 'en'    #default language is English
 
 multiL = {
     'fr': {'Story': 'Histoire',
@@ -12,18 +12,50 @@ multiL = {
            'Collection': 'Collection',
            'Settings': 'Settings',
            'Credit': 'Credit',
-           'Quit': 'Quit'}
+           'Quit': 'Quit'},
+    'de': {'Story': 'Geschichte',
+           'Versus': 'Konfrontation',
+           'Collection': 'Kollektion',
+           'Settings': 'Einstellungen',
+           'Credit': 'Verdienst',
+           'Quit': 'Weggehen'},
+    'es': {'Story': 'Historia',
+           'Versus': 'Versus',
+           'Collection': 'Colección',
+           'Settings': 'Parámetros',
+           'Credit': 'Mérito',
+           'Quit': 'Dejar'}
 }
 
 
 def setL(selectedLanguage):
+    """
+    Define the language to display
+    :param selectedLanguage: the language that is selected by the user
+    :type str
+    no return: set the language used in parameter of the set functions
+    """
     global currL
-    currL = selectedLanguage
+    if selectedLanguage in multiL:  #verify that the selected language is one of the four proposed, technically if it is one of the keys of the dictionnary
+        currL = selectedLanguage
 
 
 def sysL(argv):
+    """
+
+    :param argv:
+    :type
+    no return:
+    """
     if len(argv) >= 2:
         setL(argv[1].lower())
 
 def transL(word):
-    return multiL[currL][word]
+    """
+
+    :param word: word that we have to translate on the menu
+    :type str
+    :return:
+    :rtype str
+    """
+    return multiL[currL].get(word, '***')   #to prevent if a word is not defined
