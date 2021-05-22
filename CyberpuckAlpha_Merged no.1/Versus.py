ceb41c6e9a8a2a953b263d45865706e6a877ff01
Kunthearo <79265506+Kunthearo@users.noreply.github.com>
2021-05-22 20:41:32 +0200

    Update Versus.py

--- CyberpuckAlpha_Merged no.1/Versus.py
+++ CyberpuckAlpha_Merged no.1/Versus.py
@@ -2,8 +2,6 @@ import pygame
 import winsound
 from Application import Application
 
-from CORE.main_junction import core
-
 class Versus(Application):
     def __init__(self, game):
         Application.__init__(self, game)
@@ -12,8 +10,7 @@ class Versus(Application):
         self.mode2x, self.mode2y = self.midw + 500, self.midh - 100
         self.pointsx, self.pointsy = self.midw + 100, self.midh + 40
         self.timex, self.timey = self.midw + 500, self.midh + 40
-        self.powerx, self.powery = self.midw + 100, self.midh + 180
-        self.no_powerx, self.no_powery = self.midw + 500, self.midh + 180
+        self.nextx, self.nexty = self.midw, self.midh + 200
 
         self.back = pygame.image.load('Back_Menu.png')
 
@@ -33,11 +30,9 @@ class Versus(Application):
             self.game.draw_text("1 VS AI", 40, self.mode2x, self.mode2y)
             self.game.draw_text("Points", 40, self.pointsx, self.pointsy)
             self.game.draw_text("Time", 40, self.timex, self.timey)
-            self.game.draw_text("Alexander", 40, self.powerx, self.powery)
-            self.game.draw_text("Sanic", 40, self.no_powerx, self.no_powery)
             self.game.draw_text_3("Game Mode", 40, self.midw - 400, self.mode1y)
             self.game.draw_text_3("Type of Game", 40, self.midw - 400, self.pointsy)
-            self.game.draw_text_3("P2 Character", 40, self.midw - 400, self.powery)
+            self.game.draw_text_2("NEXT", 40, self.nextx, self.nexty + 25)
             self.draw_cursor()
             self.blit_screen()
 
@@ -55,35 +50,29 @@ class Versus(Application):
                 self.cursor_rect.midtop = (self.timex + self.offset, self.timey)
                 self.state = 'Time'
             elif self.state == 'Time':
-                self.cursor_rect.midtop = (self.powerx + self.offset, self.powery)
-                self.state = 'Enabled'
-            elif self.state == 'Enabled':
-                self.cursor_rect.midtop = (self.no_powerx + self.offset, self.no_powery)
-                self.state = 'Disabled'
-            elif self.state == 'Disabled':
+                self.cursor_rect.midtop = (self.nextx + self.offset, self.nexty)
+                self.state = 'NEXT'
+            elif self.state == 'NEXT':
                 self.cursor_rect.midtop = (self.mode1x + self.offset, self.mode1y)
                 self.state = '1 VS 1'
 
         elif self.game.UP_KEY:
             winsound.PlaySound('button-3.wav', winsound.SND_FILENAME)
             if self.state == '1 VS 1':
-                self.cursor_rect.midtop = (self.no_powerx + self.offset, self.no_powery)
-                self.state = 'Disabled'
+                self.cursor_rect.midtop = (self.nextx + self.offset, self.nexty)
+                self.state = 'NEXT'
             elif self.state == '1 VS AI':
                 self.cursor_rect.midtop = (self.mode1x + self.offset, self.mode1y)
                 self.state = '1 VS 1'
             elif self.state == 'Points':
                 self.cursor_rect.midtop = (self.mode2x + self.offset, self.mode2y)
-                self.state = '1 VS 1'
+                self.state = '1 VS AI'
             elif self.state == 'Time':
                 self.cursor_rect.midtop = (self.pointsx + self.offset, self.pointsy)
                 self.state = 'Points'
-            elif self.state == 'Enabled':
+            elif self.state == 'NEXT':
                 self.cursor_rect.midtop = (self.timex + self.offset, self.timey)
                 self.state = 'Time'
-            elif self.state == 'Disabled':
-                self.cursor_rect.midtop = (self.powerx + self.offset, self.powery)
-                self.state = 'Enabled'
 
         elif self.game.BACK_KEY:
             self.game.curr_menu = self.game.menu
@@ -94,37 +83,13 @@ class Versus(Application):
         self.move_cursor()
         if self.game.START_KEY:
             if self.state == '1 VS 1':
-                #winsound.PlaySound('button-11.wav', winsound.SND_FILENAME)
-                #self.game.playing = True
-                #self.game.curr_menu = self.game.quit
                 self.player_parameters[1][0]="PLAYER2"
             elif self.state == '1 VS AI':
-                #winsound.PlaySound('button-11.wav', winsound.SND_FILENAME)
-                #self.game.curr_menu = self.game.quit
                 self.player_parameters[1][0]="0COM2"
             elif self.state == 'Points':
-                #winsound.PlaySound('button-11.wav', winsound.SND_FILENAME)
-                #self.game.curr_menu = self.game.quit
                 self.game_parameters[0]="score_3"
             elif self.state == 'Time':
-                #winsound.PlaySound('button-11.wav', winsound.SND_FILENAME)
-                #self.game.curr_menu = self.game.quit
                 self.game_parameters[0] ="time_120"
-            elif self.state == 'Enabled':
-                #winsound.PlaySound('button-11.wav', winsound.SND_FILENAME)
-                self.player_parameters[0][1]="Alexander"
-                self.player_parameters[1][1] ="Sanic"
-                #self.game.curr_menu = self.game.quit
-            elif self.state == 'Disabled':
-                #winsound.PlaySound('button-4.wav', winsound.SND_FILENAME)
-                self.player_parameters[0][1]="Sanic"
-                self.player_parameters[1][1] ="Alexander"
-                #self.game.curr_menu = self.game.quit
+            elif self.state == 'NEXT':
+                self.game.curr_menu = self.game.selection
             self.run_display = False
-
-        if self.game.TAB_KEY:
-
-            core(self.game.system_parameters,self.player_parameters,self.game_parameters)
-
-            pygame.mixer.music.load('midnight-ride-01a.wav')
-            pygame.mixer.music.play(-1)
