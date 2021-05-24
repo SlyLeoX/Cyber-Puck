d955a47e3a55ad0a98caa9933fdc47ef8ccb5519
Kunthearo <79265506+Kunthearo@users.noreply.github.com>
2021-05-24 16:21:57 +0200

    Update Pucks.py

--- Cyberpuck_ReleaseDirectory/MENU/Pucks.py
+++ Cyberpuck_ReleaseDirectory/MENU/Pucks.py
@@ -8,10 +8,12 @@ class Pucks(Application):
         self.width, self.height = 1366, 768
         self.display = pygame.Surface((self.width,self.height))
 
+        #this will load the different images present in the folder
         self.puck1 = pygame.image.load('MENU\puck1.png')
         self.puck2 = pygame.image.load('MENU\puck2.png')
         self.back = pygame.image.load('MENU\Back_Menu.png')
 
+    #this function display_menu will help us to show the different pucks of the game
     def display_menu(self):
         self.run_display = True
         while self.run_display:
@@ -25,6 +27,7 @@ class Pucks(Application):
             self.game.draw_text_2('Pucks', 50, 300, 100)
             self.blit_screen()
 
+    #this function move_cursor can allow the user to return to the collection
     def move_cursor(self):
         if self.game.BACK_KEY:
             self.game.curr_menu = self.game.collection
