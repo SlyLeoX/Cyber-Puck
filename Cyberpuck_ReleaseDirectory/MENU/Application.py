455998a64819ae5c4cadaa63a1f6d6068d2bf140
Kunthearo <79265506+Kunthearo@users.noreply.github.com>
2021-05-24 14:45:26 +0200

    Update Application.py

--- Cyberpuck_ReleaseDirectory/MENU/Application.py
+++ Cyberpuck_ReleaseDirectory/MENU/Application.py
@@ -13,7 +13,6 @@ class Application():
 
     def draw_cursor(self):
         self.game.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
-        #pygame.draw.rect(self.game.window2, self.game.RED, (self.cursor_rect.x, self.cursor_rect.y, 200, 100), 2)
 
     def blit_screen(self):
         self.game.window.blit(self.game.display, (0, 0))
